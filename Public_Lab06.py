8db4f08a6e37ef330e3f30a3e7abeede66a47d5e
ShobhaRatnam <127446492+ShobhaRatnam@users.noreply.github.com>
2023-03-10 03:13:00 +0530

    Add files via upload

---
+++ Public_Lab06.py
@@ -0,0 +1,25 @@
+def print_menu():
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit ")
+
+def encoder(password):
+    emp_str = ""
+    for i in range(len(password)):
+        a = int(password[i])
+        b = a + 3
+        b = str(b)
+        emp_str += b
+    return emp_str
+
+def main():
+    while True:
+        print_menu()
+        print("Please enter an option:", end = ' ')
+        option = int(input())
+        if option == 1:
+            password = input("Enter your password to encode:")
+            encoded_password = encoder(password)
+            print("Your password has been encoded and stored!")
+        if option == 3:
+            break
+if __name__ == '__main__':
+    main()
